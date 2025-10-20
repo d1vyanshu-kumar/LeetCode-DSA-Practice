93af7a52039431561afbf481a1faeed1aacfe304
divyanshu_Kumar <154233802+d1vyanshu-kumar@users.noreply.github.com>
2025-10-20 15:35:45 +0530

    problem 27

---
+++ LeetCode-problems/Arrays/RemoveElement_27.java
@@ -0,0 +1,27 @@
+public class RemoveElement_27 {
+
+    public static void main(String[] args) {
+        
+        int [] nums = {2,3,4,3};
+        int val = 3;
+
+        System.out.println(removeElement(nums, val));
+    }
+
+    static int removeElement(int [] nums, int val){
+
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+
+    
+}
